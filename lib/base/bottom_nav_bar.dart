48158625a1239a3288a375adaddc685d8bcf171b
Your Name <you@example.com>
2025-08-20 18:23:56 +0100

    done with the appbar and bottom navbar

---
+++ lib/base/bottom_nav_bar.dart
@@ -0,0 +1,70 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:my_first_app/screens/home_screen.dart';
+
+class BottomNavbar extends StatefulWidget {
+  const BottomNavbar({super.key});
+
+  @override
+  State<BottomNavbar> createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> {
+  final appScreens = [
+    const HomeScreen(),
+    const Center(child: Text("search")),
+    const Center(child: Text("Tickets")),
+    const Center(child: Text("profile")),
+  ];
+
+  // ignore: unused_field
+  int _selectedIndex = 0;
+
+  // ignore: avoid_types_as_parameter_names
+  void onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: const Text("My Ticket")),
+      body: appScreens[_selectedIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: onItemTapped,
+        selectedItemColor: Colors.blueGrey,
+        unselectedItemColor: const Color(0xFF526400),
+        showSelectedLabels: false,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+            label: "home",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_search_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
+            label: "home",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+            label: "home",
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(FluentSystemIcons.ic_fluent_person_regular),
+            activeIcon: Icon(FluentSystemIcons.ic_fluent_person_filled),
+            label: "home",
+          ),
+        ],
+      ),
+    );
+  }
+}
